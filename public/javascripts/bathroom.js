bf342a295ca014391af91f5c59a8ed783d04a5cb
giuliogallerini <giulio.gallerini.work@hotmail.com>
2017-02-14 02:00:18 +0100

    sync clients

--- public/javascripts/bathroom.js
+++ public/javascripts/bathroom.js
@@ -1,17 +1,28 @@
+'use strict';
 
-debugger
 var app = window.app = {};
 var database = './src/bathroom.json';
 
-app.Bathroom = function(jsonfile){
+app.Bathroom = function(){
 	// costants
-	this.jsonfile = jsonfile;
-	this.reloadInterval = 1000 * 10;
+	this.reloadInterval = 1000 * 0.2;
 	this.timer = 15 * 60 * 1000;
 	this.interval = null;
 	// values
-	this.startAt = $('#start-at').val();
-	this.status = $('#status').val() === 'available';
+	this.state = {
+		bathroom:{
+			"state": "available",
+			"startAt": 0,
+			"currentUser": null,
+		},
+		"roommates":[
+			{"name": "Giulio"},
+			{"name":"Miriam"},
+			{"name":"Marine"},
+			{"name":"Daniele"},
+			{"name":"Romina"},
+		]
+	};
 	this.location = $('#location').val();
 	// elements
 	this._main_e = $('#bathroom');
@@ -27,29 +38,55 @@ app.Bathroom.prototype = {
 	_initializer: function(){
 		// reload the page
 		setInterval(function () {
-			console.log(this.jsonfile.readFileSync(database));
-		}, this.reloadInterval);
+			this._currentState();
+		}.bind(this), this.reloadInterval);
+
+		// change status
 		this._main_e.on('click', function(e){
 			e.preventDefault();
-			this.status = !this.status;
-			this._changeStatus(this.status);
+			var newState = {};
+			if(this.state.bathroom.state === 'available'){
+				newState = Object.assign({}, this.state, {
+					bathroom: {
+						state: 'busy',
+						startAt: Date.now(),
+						currentUser: null
+					}
+				});
+				this._updateState(newState);
+			}else{
+				newState = Object.assign({}, this.state, {
+					bathroom: {
+						state: 'available',
+						startAt: 0,
+						currentUser: null
+					}
+				});
+				this._updateState(newState);
+			}
 		}.bind(this));
 	},
 
-	_changeStatus: function(statusType){
-		if(statusType){
+	_updateUI: function(bathroom){
+		var available = bathroom.state === 'available';
+		if(available){
 			this._main_e.removeClass('busy');
 			this._main_e.addClass('available');
 			this._stopTimer();
 		}else{
 			this._main_e.removeClass('available');
 			this._main_e.addClass('busy');
-			this._startTimer();
+			this._startTimer(bathroom.startAt);
 		}
 	},
 
-	_startTimer: function(){
-		var currentTimer = this.timer;
+	_startTimer: function(startAt){
+		startAt = parseInt(startAt, 10);
+		// finding delay
+		var timerStarted = startAt !== 0 ? startAt : Date.now(); // some mock date
+		var now = Date.now();
+		var delay = now - timerStarted // calc delay
+		var currentTimer = this.timer - delay;
 		// converting timer in seconds
 		currentTimer = (currentTimer / 1000) -1;
 		this.interval = setInterval(function () {
@@ -67,4 +104,45 @@ app.Bathroom.prototype = {
 	_pauseTimer: function(){
 
 	},
+
+	_updateAppState: function(updatedState){
+		this.state = updatedState;
+		this._updateUI(this.state.bathroom);
+	},
+
+	_updateState: function(state){
+		$.ajax({
+	    type: "PATCH",
+	    url: "/state",
+	    data: JSON.stringify({
+				data: state,
+    	}),
+	    processData: false,
+	    contentType: "application/json",
+	    dataType: "json"
+  	})
+  	.done(function(state) {
+			this._updateAppState(state);
+  	}.bind(this))
+  	.fail(function(error) {
+	    console.log(error);
+  	});
+	},
+
+	_currentState: function(){
+		$.ajax({
+	    type: "GET",
+	    url: "/current-state",
+	    processData: false,
+	    contentType: "application/json",
+	    dataType: "json"
+  	})
+  	.done(function(state) {
+			if(state.bathroom.state !== this.state.bathroom.state)
+				this._updateAppState(state);
+  	}.bind(this))
+  	.fail(function(error) {
+	    console.log(error);
+  	});
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var jsonfile = require('jsonfile');
+var database = '/tmp/database.json'
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bathroom', jsonfile: jsonfile });
+	res.render('index', { title: 'Bathroom'});
+});
+
+router.patch('/state', function(req, res, next) {
+	var state = req.body.data;
+	jsonfile.writeFileSync(database, state)
+  res.contentType('json');
+  res.send(JSON.stringify(state));
+});
+
+router.get('/current-state', function(req, res, next) {
+	jsonfile.readFile(database, function(err, state) {
+	  res.contentType('json');
+	  res.send(JSON.stringify(state));
+	});
 });
 
 module.exports = router;
